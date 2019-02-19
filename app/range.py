dab181ee88a6af4ce4497f7971d4b3931e427bba
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-02-19 10:39:17 +0300

    Merge branch 'master' of C:\HomeWorks\Playground with conflicts.

--- app/range.py
+++ app/range.py
@@ -5,21 +5,3 @@ print(list_of_months(3))
 months = [((month % 12) + 1) for month in range(2, 14)]
 
 print(months)
-
-squares = [value**2 for value in range(10)]
-
-print(squares)
-roots = []
-for square in squares:
-    roots.append(square**0.5)
-print(roots)
-
-
-odd_numbers = [k * 2 + 1 for k in range(25)]
-print(odd_numbers[0:40])
-
-multiples_of_three = [k * 3 for k in range(1, 31)]
-print(multiples_of_three)
-
-list_of_cubes = [k ** 3 for k in range(1, 11)]
-print(list_of_cubes)
