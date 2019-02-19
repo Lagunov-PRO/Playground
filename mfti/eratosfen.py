c4ec9895b5e0586fe15232e1b6fff232743693fc
Lagunov-PRO-Mac <alexey@lagunov.pro>
2019-02-19 08:03:42 +0300

    Simplified composing the string with +=

--- mfti/eratosfen.py
+++ mfti/eratosfen.py
@@ -1,11 +1,20 @@
-n = 50
-a = [True] * n
-a[0] = a[1] = False
-for k in range(n - 1):
-    if a[k]:
-        for m in range(2*k, n, k):
-            a[m] = False
+upper_limit = 1_000
+A = [True] * upper_limit
+A[0] = A[1] = False  # 0 and 1 are not primes
+numbers = [k for k in range(upper_limit)]
+# print(numbers)
+for k in range(upper_limit - 1):
+    rejected = []
+    if A[k]:
+        for m in range(2*k, upper_limit, k):
+            rejected.append(m)
+            A[m] = False
+    if rejected:
+        print('Делитель {}, отброшено {}'.format(k, len(rejected)))
 
-for k in range(n):
-    if a[k]:
-        print(k, '—', 'простое')
+primes = [k for k in range(upper_limit) if A[k]]
+
+print(primes)
+print(len(primes))
+
+
