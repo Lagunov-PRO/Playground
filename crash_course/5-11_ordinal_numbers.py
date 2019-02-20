e48cf5eb62f7c1fd96c0422b17ed1b4c2e53a9e7
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-02-20 09:19:47 +0300

    Updates with conflicts

--- crash_course/5-11_ordinal_numbers.py
+++ crash_course/5-11_ordinal_numbers.py
@@ -6,10 +6,10 @@ ordinal_numbers = []
 for number in numbers:
     if number == 1:
         ordinal_numbers.append('1st')
-    if number == 2:
+    elif number == 2:
         ordinal_numbers.append('2nd')
-    if number == 3:
+    elif number == 3:
         ordinal_numbers.append('3rd')
-    else:
-        ordinal_numbers.append('th')
+    elif number > 3:
+        ordinal_numbers.append(str(number) + 'th')
 print(ordinal_numbers)
