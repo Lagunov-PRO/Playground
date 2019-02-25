ca768956c5504be798f51207442764694e7da817
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-02-25 18:41:57 +0300

    New exercises

---
+++ 02_ crash_course/8-0_midle_name.py
@@ -0,0 +1,13 @@
+def get_formatted_name(first_name, last_name, middle_name=''):
+    """Return a full name, neatly formatted."""
+    if middle_name:
+        full_name = '{} {} {}'.format(first_name.title(), middle_name.title(), last_name.title())
+    else:
+        full_name = '{} {}'.format(first_name.title(), last_name.title())
+    return full_name.title()
+
+
+musician = get_formatted_name('jimi', 'hendrix')
+print(musician)
+musician = get_formatted_name('john', 'hooker', 'lee')
+print(musician)
