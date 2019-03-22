ecddc15e9bd6fc3dc78169bf38fbe2ebd4d324e4
Lagunov-PRO-Work <alexey@lagunov.pro>
2019-03-22 10:31:31 +0300

    Moved to pythonic solutions

--- 01_own_apps/07_relative_to_absolute_path.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-work_dir = Path(__file__).parent
-relative_data_path = '../data'
-zodiacs_en_filename = 'zodiacs_en.txt'
-zodiacs_ru_filename = 'zodiacs_ru.txt'
-zodiacs_en = (work_dir / relative_data_path / zodiacs_en_filename).resolve()
-zodiacs_ru = (work_dir / relative_data_path / zodiacs_ru_filename).resolve()
